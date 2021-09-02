91f04f9d3c95431cd057b79d1e784f1c26f839d4
hann <hmweeks93@gmail.com>
2021-09-02 14:13:57 -0500

    added id to user serializer

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -330,7 +330,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     """
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('id', 'first_name', 'last_name', 'email')
         depth = 1
 
 
